777dcb9a6cf42ede3b716f59f42177303d8db2f2
Jakub-Ciezki <115064129+Jakub-Ciezki@users.noreply.github.com>
2024-02-26 11:54:10 +0100

    half-done
    do your fracking job

--- 2tp/Slidergry/slajdgry.js
+++ 2tp/Slidergry/slajdgry.js
@@ -1,25 +1,95 @@
-function sprawdz()
-{
-	var liczba = document.getElementById("pole").value;
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-3.7.1.min.js'; // Check https://jquery.com/ for the current version
+document.getElementsByTagName('head')[0].appendChild(script);
 
-	if(liczba > 0) {
-		document.getElementById("wart_wyjsc").innerHTML = "Liczba jest dodatnia!";
-	} else if(liczba == ":3" || liczba == ";3") {
-		document.getElementById("wart_wyjsc").innerHTML = ":33333";
-	} else if(liczba == 0) {
-		document.getElementById("wart_wyjsc").innerHTML = "Liczba jest równa zero!";
-	} else if(liczba < 0) {
-		document.getElementById("wart_wyjsc").innerHTML = "Liczba jest ujemna!";
-	}
+var numer = Math.floor(Math.random()*5)+1;
+			
+			var timer1 = 0;
+			var timer2 = 0;
+			
+			function ustawslajd(nrslajdu)
+			{
+				clearTimeout(timer1);
+				clearTimeout(timer2);
+				numer = nrslajdu - 1;
+				
+				schowaj();
+				setTimeout("zmienslajd()", 500);
 
-	
-}
-function dodaj()
-{
-	var liczA = document.getElementById("pole").value;
-	var liczB = document.getElementById("pole_dodaj").value;
+			}
 
-	var wynik_dodaj = Number(liczA) + Number(liczB);
+			function ustawHK(nrslajdu)
+			{
+				clearTimeout(timer1);
+				clearTimeout(timer2);
+				numer = nrslajdu - 1;
+				
+				schowaj();
+				setTimeout("zmienHK()", 500);
 
-	document.getElementById("wart_wyjsc").innerHTML = wynik_dodaj;
-}
+			}
+
+			function ustawDMC(nrslajdu)
+			{
+				DMCstop = false;
+
+				clearTimeout(timer1);
+				clearTimeout(timer2);
+				numer = nrslajdu - 1;
+				
+				schowaj();
+				setTimeout("zmienDMC()", 500);
+
+			}
+			
+			function schowaj()
+			{
+				$("#slider").fadeOut(1000);
+			}
+		
+			function zmienslajd()
+			{
+
+				
+				numer++; if (numer>5) numer=1;
+				
+				var plik = "<img src=\"slajdy/slajd" + numer + ".png\" />";
+				
+				document.getElementById("slider").innerHTML = plik;
+				$("#slider").fadeIn(500);
+				
+				timer1 = setTimeout("zmienslajd()", 7000);
+				timer2 = setTimeout("schowaj()", 6000);
+			
+			}
+
+			function zmienHK()
+			{
+
+
+				numer++; if (numer>6) numer=1;
+				
+				var plik = "<img src=\"slajdyHK/slajd" + numer + ".png\" />";
+				
+				document.getElementById("slider").innerHTML = plik;
+				$("#slider").fadeIn(500);
+				
+				timer1 = setTimeout("zmienHK()", 7000);
+				timer2 = setTimeout("schowaj()", 6000);
+			
+			}
+
+			function zmienDMC()
+			{
+
+				numer++; if (numer>7) numer=1;
+				
+				var plik = "<img src=\"slajdyDMC/slajd" + numer + ".png\" />";
+				
+				document.getElementById("slider").innerHTML = plik;
+				$("#slider").fadeIn(500);
+				
+				timer1 = setTimeout("zmienDMC()", 7000);
+				timer2 = setTimeout("schowaj()", 6000);
+			
+			}
