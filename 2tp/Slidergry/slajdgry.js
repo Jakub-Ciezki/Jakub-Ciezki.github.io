f02a6c40140ed1bc90f97339a420855242782ab6
Jakub-Ciezki <115064129+Jakub-Ciezki@users.noreply.github.com>
2024-03-01 10:08:55 +0100

    adadaddadsasdfgn
    sex

--- 2tp/Slidergry/slajdgry.js
+++ 2tp/Slidergry/slajdgry.js
@@ -2,6 +2,45 @@ var script = document.createElement('script');
 script.src = 'http://code.jquery.com/jquery-3.7.1.min.js'; // Check https://jquery.com/ for the current version
 document.getElementsByTagName('head')[0].appendChild(script);
 
+function shawHK() {
+var slajdd = document.getElementById("btnslajd");
+var hollow = document.getElementById("btnHK");
+var DemonC = document.getElementById("btnDMC");
+
+	slajdd.style.display = "none";
+	DemonC.style.display = "none";
+
+	hollow.style.display = "block";
+	ustawHK(1);
+}
+function showDMC() {
+	var slajdd = document.getElementById("btnslajd");
+	var hollow = document.getElementById("btnHK");
+	var DemonC = document.getElementById("btnDMC");
+
+	slajdd.style.display = "none";
+	hollow.style.display = "none";
+
+	DemonC.style.display = "block";
+	ustawDMC(1);
+}
+function showslajd() {
+	var slajdd = document.getElementById("btnslajd");
+	var hollow = document.getElementById("btnHK");
+	var DemonC = document.getElementById("btnDMC");
+
+	hollow.style.display = "none";
+	DemonC.style.display = "none";
+
+	slajdd.style.display = "block";
+	ustawslajd(1);
+}
+
+function scrollbtm() {
+	window.scrollTo(0, document.body.scrollHeight);
+}
+
+
 var numer = Math.floor(Math.random()*5)+1;
 			
 			var timer1 = 0;
