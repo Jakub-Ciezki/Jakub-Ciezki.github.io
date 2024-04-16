501311c646865d908b04b13470c85ea32f313326
Jakub-Ciezki <115064129+Jakub-Ciezki@users.noreply.github.com>
2024-04-16 14:06:15 +0200

    zjabałem

---
+++ 2tp/kartkowkaJS/kart.js
@@ -0,0 +1,9 @@
+function znakNa()
+{
+    
+    var charAt_in = document.getElementById("charAt_IN").innerHTML;
+
+    var znaki = 
+
+    document.getElementById("OUT_charAt").innerHTML = znaki;
+}
